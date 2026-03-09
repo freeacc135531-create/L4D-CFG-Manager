8e1c429c431d4917fe3a2e9552e9bb9f36500521
paradox32000 <freeacc135531@gmail.com>
2026-03-09 17:22:12 +0100

    Delete L4D CFG Manager directory

--- L4D CFG Manager/ui/scripts_tab.py
+++
@@ -1,9 +0,0 @@
-from tkinter import ttk
-
-
-class ScriptsTab(ttk.Frame):
-
-    def __init__(self, notebook):
-        super().__init__(notebook)
-
-        ttk.Label(self, text="Script Generator Coming Soon").pack(pady=20)
