916291799df245ae1837844f0a94cc5639cb12d1
paradox32000 <freeacc135531@gmail.com>
2026-03-15 00:53:01 +0100

    Delete data directory

--- data/tooltips.py
+++
@@ -1,7 +0,0 @@
-TOOLTIPS = {
-    "rate": "Max bandwidth (100000 recommended for L4D2)",
-    "cl_interp": "Interpolation time (0.033 competitive)",
-    "cl_cmdrate": "Packets sent per second",
-    "cl_updaterate": "Packets received per second",
-    "fps_max": "FPS limit (0 = unlimited)"
-}
